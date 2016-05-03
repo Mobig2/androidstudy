3c861eef0c9d7047d329677d691ee350bc8634be
rlqha <rlqha123@gmail.com>
2016-05-03 10:15:48 +0900

    Ex06_MyEditText2 - > 문자입력 수 제한 / 숫자를 입력받았을때 ' , '로 구분 자동 엔터는 구현안돼있음

--- Ex06_MyEditText2/src/com/example/ex06_myedittext2/MainActivity.java
+++ Ex06_MyEditText2/src/com/example/ex06_myedittext2/MainActivity.java
@@ -1,34 +1,56 @@
 package com.example.ex06_myedittext2;
 
+import java.text.DecimalFormat;
+
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
+	EditText inputMessage;
+	String strAmount = ""; //임시 저장값 (콤마)
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_activity);
+		
+		inputMessage = (EditText)findViewById(R.id.inputMessage);
+		
+		//inputMessage.setInputType(InputType.TYPE_CLASS_NUMBER);//숫자만입력
+		inputMessage.addTextChangedListener(watcher);
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+	
+	TextWatcher watcher = new TextWatcher() {
+		public void onTextChanged(CharSequence str, int start, int before, int count){
+			Log.d("gibom", str.toString() + ":" + strAmount);
+			if(!str.toString().equals(strAmount)){ //StackOverflow를 막기위해,(문자입력받으려면 이 조건문을 주석처리해야됨)
+				strAmount = makeStringComma(str.toString().replace(",", ""));
+				inputMessage.setText(strAmount);
+				
+				inputMessage.setSelection(inputMessage.getText().length(), 
+										  inputMessage.getText().length());			
+			}
 		}
-		return super.onOptionsItemSelected(item);
+		public void beforeTextChanged(CharSequence str, int start, int count, int after){
+			strAmount = str.toString();
+		}
+		
+		public void afterTextChanged(Editable strEditable){
+			
+		}
+	};
+
+	protected String makeStringComma(String str){
+		if(str.length() == 0)
+			return "";
+		long value = Long.parseLong(str);
+		DecimalFormat format = new DecimalFormat("###,##0");
+		return format.format(value);
 	}
+	
 }
